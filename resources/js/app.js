0f801d27c4dec4e9e119d843fb88bc71eb705bb1
lercikk <sinigur.valerica@elev.cihcahul.md>
2024-03-13 09:32:19 +0200

    Lucrarea cu carosela swiper

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
 import './bootstrap';
 import * as bootstrap from 'bootstrap';
+
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
 import "../css/app.css";
--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -19,5 +19,25 @@
     </div>
 
 </header>
+<section class="auctions">
+    <div class="title d-flex justify-content-between align-items-center">
+        <h2 class="text-light">Live Auctions </h2>
+
+            <a href="#" class="btn btn-bg-light text-white px-4 py-2 me-3">Explore All</a>
+    </div>
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">Slide 1</div>
+          <div class="swiper-slide">Slide 2</div>
+          <div class="swiper-slide">Slide 3</div>
+          <div class="swiper-slide">Slide 4</div>
+          <div class="swiper-slide">Slide 5</div>
+          <div class="swiper-slide">Slide 6</div>
+          <div class="swiper-slide">Slide 7</div>
+          <div class="swiper-slide">Slide 8</div>
+          <div class="swiper-slide">Slide 9</div>
+        </div>
+        <div class="swiper-pagination"></div>
+</section>
 
 @endsection
